8bee2867dd989f4b221fb509d8cd301707642531
Vvvaishnavii <vaishnavikachhawaha@gmail.com>
2025-04-22 15:58:56 +0530

    Engine banayi hai

--- Assets/Scripts/PartSnapZone.cs
+++ Assets/Scripts/PartSnapZone.cs
@@ -1,25 +1,34 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PartSnapZone : MonoBehaviour
 {
-   public string expectedTag;
-    public GameObject assembledPart; // The fixed version (initially inactive)
+  [System.Serializable]
+    public class PartInfo
+    {
+        public string expectedTag;
+        public GameObject assembledPart; // The fixed version (initially inactive)
+    }
+
+    public List<PartInfo> partsList;
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Triggered by: " + other.name);
 
-        if (other.CompareTag(expectedTag))
+        foreach (PartInfo part in partsList)
         {
-            Debug.Log("Correct part dropped!");
+            if (other.CompareTag(part.expectedTag))
+            {
+                Debug.Log("Correct part dropped!");
 
-            other.gameObject.SetActive(false); // Hide disassembled
-            assembledPart.SetActive(true);     // Show assembled
+                other.gameObject.SetActive(false); // Hide disassembled
+                part.assembledPart.SetActive(true); // Show assembled
+                return; // Exit after first match
+            }
         }
-        else
-        {
-            Debug.Log("Wrong part. Expected: " + expectedTag + ", Got: " + other.tag);
-        }
+
+        Debug.Log("Wrong part. No matching expectedTag found for: " + other.tag);
     }
 }
