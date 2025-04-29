7e18b277ebbd097101542c489adbbf33a1ff0fb7
Vvvaishnavii <vaishnavikachhawaha@gmail.com>
2025-04-29 10:58:07 +0530

    trying man trying

--- Assets/VRObjectSpawner.cs
+++ Assets/VRObjectSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -6,13 +7,40 @@ public class VRObjectSpawner : MonoBehaviour
     public GameObject prefabToSpawn;
     public Transform handAttachPoint;
 
-    public void SpawnObject(XRBaseInteractor interactor)
-    {
-        GameObject spawned = Instantiate(prefabToSpawn);
-        spawned.transform.position = handAttachPoint.position;
-        spawned.transform.rotation = handAttachPoint.rotation;
+    private XRGrabInteractable grabInteractable;
 
-        var grabInteractable = spawned.GetComponent<XRGrabInteractable>();
-        interactor.interactionManager.SelectEnter(interactor, grabInteractable);
+    private void Awake()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.selectEntered.AddListener(OnGrab);
     }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        XRBaseInteractor interactor = args.interactorObject as XRBaseInteractor;
+        if (interactor != null)
+        {
+            SpawnObject(interactor);
+        }
+    }
+
+public void SpawnObject(XRBaseInteractor interactor)
+{
+    GameObject spawned = Instantiate(prefabToSpawn);
+    spawned.transform.position = handAttachPoint.position;
+    spawned.transform.rotation = handAttachPoint.rotation;
+
+    var grabInteractable = spawned.GetComponent<XRGrabInteractable>();
+    if (grabInteractable != null)
+    {
+        StartCoroutine(WaitAndGrab(interactor, grabInteractable));
+    }
+}
+
+private IEnumerator WaitAndGrab(XRBaseInteractor interactor, XRGrabInteractable grabInteractable)
+{
+    yield return null; // wait one frame
+    interactor.interactionManager.SelectEnter(interactor, grabInteractable);
+}
+
 }
