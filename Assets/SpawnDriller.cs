5d99c87ea2b07ddc2b00f69184d7b9ce60c032fa
Souramya25 <souramyamandloi@gmail.com>
2025-05-01 16:33:31 +0530

    ShydUI

--- Assets/SpawnDriller.cs
+++ Assets/SpawnDriller.cs
@@ -4,20 +4,25 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class SpawnDriller : MonoBehaviour
 {
     public GameObject drillerPrefab;
-    public Transform rightHandAttachPoint;  // Drag your right-hand controller here!
+    public Transform rightHandAttachPoint;  // Drag your right-hand controller's attach point here!
 
-    public void SpawnInHand()
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the collider belongs to the right hand interactor
+        if (other.GetComponent<XRDirectInteractor>())
+        {
+            SpawnInHand(other.GetComponent<XRDirectInteractor>());
+        }
+    }
+
+    public void SpawnInHand(XRDirectInteractor interactor)
     {
         GameObject driller = Instantiate(drillerPrefab, rightHandAttachPoint.position, rightHandAttachPoint.rotation);
         XRGrabInteractable interactable = driller.GetComponent<XRGrabInteractable>();
 
         if (interactable != null)
         {
-            XRDirectInteractor interactor = rightHandAttachPoint.GetComponentInParent<XRDirectInteractor>();
-            if (interactor != null)
-            {
-                interactor.interactionManager.SelectEnter(interactor, interactable);  // Attach to hand immediately
-            }
+            interactor.interactionManager.SelectEnter(interactor, interactable);  // Attach to hand immediately
         }
     }
 }
